e5733af6d12d81b8bd7998a7c22d461c8096e4c2
macluiggy <ferrinluiggy@gmail.com>
2021-09-03 09:49:27 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Icon } from '@iconify/react';
+//<Icon icon='twemoji:tomato' />
+import { Provider } from 'react-redux';
+//import store from './store.js';
+function App() {
+  return (
+    <Provider >
+    hola
+    </Provider>
+  );
+}
+
+export default App;
