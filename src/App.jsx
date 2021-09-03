e45127a970393e7581c5d1cdcd2568da9ac5f14a
macluiggy <ferrinluiggy@gmail.com>
2021-09-03 10:45:18 -0500

    jdjd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 //import { Icon } from '@iconify/react';
 //<Icon icon='twemoji:tomato' />
+import Main from './components/Main';
 function App() {
   return (
     <div className="App">
-      holed
+      <Main />
     </div>
   );
 }
