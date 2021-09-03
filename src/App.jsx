8f1ed30ae62bc05e73d69b790e719165a5fb1086
macluiggy <ferrinluiggy@gmail.com>
2021-09-03 11:09:55 -0500

    dkkd

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import './App.css';
 //import { Icon } from '@iconify/react';
 //<Icon icon='twemoji:tomato' />
 import Main from './components/Main';
+import { Provider } from 'react-redux';
+import store from './store.js';
+
 function App() {
   return (
-    <div className="App">
+    <Provider store={store} >
       <Main />
-    </div>
+    </Provider>
   );
 }
 console.log(eval('5+5'))
