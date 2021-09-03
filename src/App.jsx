70cbe514240d70ce1b1753ea631ca0f9e0015b0e
macluiggy <ferrinluiggy@gmail.com>
2021-09-03 10:05:52 -0500

    djjd

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import { Icon } from '@iconify/react';
 //<Icon icon='twemoji:tomato' />
-import { Provider } from 'react-redux';
-//import store from './store.js';
 function App() {
   return (
-    <Provider >
-    hola
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+    
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
